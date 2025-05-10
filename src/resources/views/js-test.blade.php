ef6023acdb747ef7d8594162d80a5a5b51554945
koharu <le2.lsb108@gmail.com>
2025-05-10 13:50:57 +0900

    JSテスト用ページ作成

---
+++ src/resources/views/js-test.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/sanitize.css">
+    <title>JSテストページ</title>
+</head>
+<body>
+    <h1>JavaScriptテスト</h1>
+
+    <button onclick="sayHello()">クリックして挨拶</button>
+
+    <script src="/js/test.js"></script>
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// JSテストページ表示用
+Route::get('/js-test', function () {
+    return view('js-test');
 });
