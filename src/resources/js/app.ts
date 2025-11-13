36064859e1233db6d88d264e9a6084b2f5989ea3
koharu <le2.lsb108@gmail.com>
2025-11-13 14:21:37 +0900

    docker-compose.yml修正（nodeコマンド）

--- src/resources/js/app.ts
+++ src/resources/js/app.ts
@@ -1,7 +1,16 @@
 import './bootstrap';
 
+console.log("app.ts loaded");
+
 const button = document.querySelector<HTMLButtonElement>("#hello-button");
 const result = document.querySelector<HTMLDivElement>("#hello-result");
+
+if (!button) {
+    console.warn("ボタン要素が見つかりません (#hello-button)");
+}
+if (!result) {
+    console.warn("結果表示用の要素が見つかりません (#hello-result)");
+}
 
 if (button && result) {
     button.addEventListener("click", () => {
